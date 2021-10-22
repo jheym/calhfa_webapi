096a51ce70ad915262af89351b9c4b717921ed78
davidenzler <73456941+davidenzler@users.noreply.github.com>
2021-10-21 20:56:00 -0700

    added a count method to LoanStatusController

--- calhfa_webapi/Controllers/LoanStatusController.cs
+++ calhfa_webapi/Controllers/LoanStatusController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using calhfa_webapi.Models;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace calhfa_webapi.Controllers
 {
@@ -41,6 +43,32 @@ namespace calhfa_webapi.Controllers
             return loanStatus;
 
 
+        }
+
+        [Route("count")]
+        [HttpGet]
+        public async Task<string> GetLoanCountAsync()
+        {
+            var ComplianceReview = await _context.LoanStatuses.Where(l => l.StatusCode == 110 ||
+                                                                                    l.StatusCode == 210 ||
+                                                                                    l.StatusCode == 310 ||
+                                                                                    l.StatusCode == 410).ToListAsync();
+            var ComplianceSuspenseReview = await _context.LoanStatuses.Where(l => l.StatusCode == 422).ToListAsync();
+            var PurchaseReview = await _context.LoanStatuses.Where(l => l.StatusCode == 510 ||
+                                                                                    l.StatusCode == 810).ToListAsync();
+            var PurchaseSuspenseReview = await _context.LoanStatuses.Where(l => l.StatusCode == 522).ToListAsync();
+
+            int complianceReviewCount = ComplianceReview.Count;
+            int complianceSuspenseReviewCount = ComplianceSuspenseReview.Count;
+            int purchaseReviewCount = PurchaseReview.Count;
+            int purchaseSuspenseReviewCount = PurchaseSuspenseReview.Count;
+
+            Console.WriteLine(complianceReviewCount);
+
+            string jsonData = String.Format("{{compliantReviewCount: '{0}', compliantSuspenseCount: '{1}', purchaseReviewCount: '{2}', purchaseSuspenseCount: '{3}'}}",
+                complianceReviewCount, complianceSuspenseReviewCount, purchaseReviewCount, purchaseSuspenseReviewCount);
+
+            return Newtonsoft.Json.JsonConvert.SerializeObject(jsonData);
         }
     }
 }
